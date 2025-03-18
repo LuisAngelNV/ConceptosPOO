d7d389a5a2017a11f5b28e105cffa394a9b7a1d6
LuisAngelNV <luis_@M0KUM4>
2025-03-17 23:50:58 -0600

    Agregar archivos de proyecto.

---
+++ ConceptosPOO/Auto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceptosPOO
+{
+    public class Auto
+    {
+        public string marca;
+        public string modelo;
+        public int año;
+
+        // Método para mostrar la información del auto
+        public void MostrarInfo()
+        {
+            Console.WriteLine("Marca: " + marca);
+            Console.WriteLine("Modelo: " + modelo);
+            Console.WriteLine("Año: " + año);
+        }
+    }
+}
---
+++ ConceptosPOO/Program.cs
@@ -0,0 +1,11 @@
+﻿using ConceptosPOO;
+
+
+Auto miAuto = new Auto();
+// Asignar valores a los atributos del objeto
+miAuto.marca = "Toyota";
+miAuto.modelo = "Corolla";
+miAuto.año = 2022;
+
+// Llamar al método MostrarInfo()
+miAuto.MostrarInfo();
